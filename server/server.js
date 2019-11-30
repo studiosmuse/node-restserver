cdb052dec45795af94341c283507a7f971cafa78
studios muse <elstudiosmuse@gmail.com>
2019-11-30 00:03:19 -0300

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -12,46 +14,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
- //GET
-app.get('/usuario', function (req, res) {
-  res.json('get Usuario');
-});
+app.use(require('./routes/usuario'));
+ 
+// mongoose.connect('mongodb://localhost:27017/cafe',
+    mongoose.connect(process.env.urlDB,   
+    { useNewUrlParser: true, useCreateIndex: true },
 
-//POST
-app.post('/usuario', function (req, res) {
+    (err, res) => {
+
+
+    if(err) throw err;
     
-    let body = req.body;
+    console.log('Base de Datos Online');
 
-    if(body.nombre === undefined) {
-       res.status(400).json({
-          ok: false,
-          mensaje: 'El nombre es necesario'
-       });
-
-    }else{
-
-        res.json({
-            persona: body
-        });
-
-    }
-
-    
-});
-
-//PUT
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-//DELETE
-app.delete('/usuario', function (req, res) {
-    res.json('delete Usuario');
 });
  
 app.listen(process.env.PORT, () => {
